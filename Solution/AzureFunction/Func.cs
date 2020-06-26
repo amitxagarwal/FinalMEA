ec82e5b6035bddea53330af957e5e5cfdb9efd6d
Dommaraju Umamaheswari <DU00556591@INBASDLP06496>
2020-06-26 11:40:01 +0530

    changes done

---
+++ Solution/AzureFunction/Func.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Azure.WebJobs;
+using System.Net.Http;
+using Microsoft.Build.Utilities;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AzureFunction
+{
+    public class Func
+    {
+        private readonly ExecutionContext _context;
+        public Func(ExecutionContext context)
+        {
+            _context = context;
+        }
+        public JToken GetJsonFromFolder()
+        {
+
+            //from folder
+            var path = System.IO.Path.Combine(_context.FunctionDirectory, "jsconfig1.json");
+            var p = Path.GetFullPath(Path.Combine(_context.FunctionDirectory, "..\\jsconfig1.json"));
+            string st = File.ReadAllText(@p);
+            return st;
+        }
+        public async Task<JToken> GetJsonFromUrl()
+        {
+
+            //from url
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://kmd-momentum-mea-internal-webapp.azurewebsites.net/swagger/v1/swagger.json");
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+
+    }
+}
--- Solution/AzureFunction/Function1.cs
+++ Solution/AzureFunction/Function1.cs
@@ -1,16 +1,23 @@
 using System;
+using System.IO;
+using System.Net.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AzureFunction
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            Func func = new Func(context);
+            func.GetJsonFromFolder();
+            func.GetJsonFromUrl();
         }
     }
 }
