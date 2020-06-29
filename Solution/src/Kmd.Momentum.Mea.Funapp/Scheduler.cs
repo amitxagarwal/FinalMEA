b34126809d392157907940041519fd13bdffd5e3
Ajay Aggarwal <QDQ@kmd.dk>
2020-06-29 12:21:58 +0530

    commit

--- Solution/src/Kmd.Momentum.Mea.Funapp/Scheduler.cs
+++ Solution/src/Kmd.Momentum.Mea.Funapp/Scheduler.cs
@@ -10,8 +10,6 @@ namespace Kmd.Momentum.Mea.Funapp
         public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            var _compareSwager = new CompareSwagger(context);
-            _compareSwager.CompareJson().ConfigureAwait(false);
         }
     }
 }
