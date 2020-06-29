f68676482396c0e8059313a59e551f248f7b6a29
Ajay Aggarwal <QDQ@kmd.dk>
2020-06-29 12:01:26 +0530

    commit

---
+++ Solution/src/Kmd.Momentum.Mea.Funapp/CompareSwagger.cs
@@ -0,0 +1,76 @@
+﻿using Kmd.Momentum.Mea.Funapp.Model;
+using Microsoft.Azure.WebJobs;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Kmd.Momentum.Mea.Funapp
+{
+    class CompareSwagger
+    {
+        private readonly Config _config;
+
+        private readonly ExecutionContext _context;
+
+        public CompareSwagger(ExecutionContext context)
+        {
+            _context = context;
+            _config = GetConfig();
+        }
+
+        public async Task CompareJson()
+        {
+            var baseJson = await ReadUrl(_config.RemotePath);
+            var remoteJson = ReadFile(_config.BasePath);
+            if (string.IsNullOrEmpty(baseJson))
+            {
+                //baseJson is Empty
+                return;
+            }
+            if (string.IsNullOrEmpty(remoteJson))
+            {
+                //remoteJson is Empty
+                return;
+            }
+            if (baseJson == remoteJson)
+            {
+                //Mea Application is using latest Mca Application
+                return;
+            }
+
+            _config.ApiList
+
+
+        }
+
+        private string ReadFile(string path)
+        {
+            var _path = Path.Combine(_context.FunctionDirectory, path);
+            var _fullPath = Path.GetFullPath(_path);
+            var data = File.ReadAllText(_fullPath);
+            return data;
+        }
+
+        private Config GetConfig()
+        {
+            var data = ReadFile("../config.json");
+            return JsonConvert.DeserializeObject<Config>(data);
+        }
+
+        private async Task<string> ReadUrl(string uri)
+        {
+            var data = string.Empty;
+
+            var client = new HttpClient();
+            var response = await client.GetAsync(uri);
+            if (response != null && response.StatusCode == HttpStatusCode.OK)
+            {
+                data = await response.Content.ReadAsStringAsync();
+            }
+
+            return data;
+        }
+    }
+}
--- Solution/src/Kmd.Momentum.Mea.Funapp/Scheduler.cs
+++ Solution/src/Kmd.Momentum.Mea.Funapp/Scheduler.cs
@@ -7,9 +7,11 @@ namespace Kmd.Momentum.Mea.Funapp
     public static class Scheduler
     {
         [FunctionName("CompareApplication")]
-        public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var _compareSwager = new CompareSwagger(context);
+            _compareSwager.CompareJson().ConfigureAwait(false);
         }
     }
 }
