d79fcbf9c13893418c72feb78311675a5a85ddf6
Dommaraju Umamaheswari <DU00556591@INBASDLP06496>
2020-06-30 09:29:32 +0530

    chanes done

--- Solution/src/Kmd.Momentum.Mea.Funapp/Scheduler.cs
+++ Solution/src/Kmd.Momentum.Mea.Funapp/Scheduler.cs
@@ -9,16 +9,12 @@ namespace Kmd.Momentum.Mea.Funapp
     public static class Scheduler
     {
         [FunctionName("CompareApplication")]
-        public static string Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log, ExecutionContext context)
-        //public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
+        public static void Run([TimerTrigger("0 */3 * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             var _compareSwager = new CompareSwagger(context);
             _compareSwager.CompareJson().ConfigureAwait(false);
 
-            return ("response");
         }
     }
 }
